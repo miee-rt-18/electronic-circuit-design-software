9b11b29162f1b35066c4e81c2af7bebf1d7dd1a7
lazba <73595400+lazbaphilipp@users.noreply.github.com>
2022-10-20 01:29:52 +0300

    Update s2pImport.m

--- import_s2p/s2pImport.m
+++ import_s2p/s2pImport.m
@@ -1,5 +1,14 @@
 function result = s2pImport(filename)
 
+% spFile - это матрица S параметров вида 
+% частота S11 S21 S12 S22
 
-result = {};
+freqs = spFile(:,1);
+spLine = spFile(:,2:end); SP = zeros(2,2,size(spLine,1));
+
+for ind = 1:1:size(spLine,1)
+    SP(:,:,ind) = reshape(spLine(ind,:),2,2);
+end; clear ind;
+
+result = {freqs;SP};
 end
